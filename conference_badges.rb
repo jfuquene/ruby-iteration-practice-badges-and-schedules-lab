15177375d1f7e330c45be4ad2f4207f886c383e6
John Fuquene <jfuquene@email.wm.edu>
2021-01-17 18:51:43 -0500

    commit tree b15119b38844a08b68b2360625cec5142ad2d7d3

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+
+#speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end 
+
+#speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(names)
+    names.map { |name| badge_maker(name) }
+end 
+
+def assign_rooms(speakers)
+    #new_arr = []
+    speakers.map.each_with_index do |name, index| 
+         "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end 
+end 
+
+def printer(attendees)
+     batch_badge_creator(attendees).each do |badge| 
+        puts badge end 
+        assign_rooms(attendees).each do |room| 
+            puts room
+        end 
+
+end 
